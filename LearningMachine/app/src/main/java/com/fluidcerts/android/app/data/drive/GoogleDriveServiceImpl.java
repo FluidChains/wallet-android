e04ddb690bc41aeca13aa78e062bd4d6dc86f9ab
Karim Boucher <boucherkarim@gmail.com>
2020-05-13 16:45:29 -0400

    Add missing dependency

--- LearningMachine/app/src/main/java/com/fluidcerts/android/app/data/drive/GoogleDriveServiceImpl.java
+++ LearningMachine/app/src/main/java/com/fluidcerts/android/app/data/drive/GoogleDriveServiceImpl.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Observable;
