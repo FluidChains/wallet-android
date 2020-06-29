87352e3f7561aff91b86eb5855d7e782b4e033ad
Karim Boucher <boucherkarim@gmail.com>
2020-06-29 17:52:18 -0400

    Add lock screen activity

---
+++ LearningMachine/app/src/main/java/com/fluidcerts/android/app/ui/lock/LockScreenActivity.java
@@ -0,0 +1,13 @@
+package com.fluidcerts.android.app.ui.lock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
